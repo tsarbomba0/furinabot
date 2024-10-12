d32dc023fc2bb2821e10379fbdfdd6f3e1329d8f
tsarbomba0 <creepermaster000@gmail.com>
2024-10-12 23:40:42 +0200

    Working command to write to database

--- handlers/command_handler.ts
+++ handlers/command_handler.ts
@@ -19,16 +19,14 @@ for (let folder in cmdfolder){
 export function interaction_handler(client){
     client.on(Events.InteractionCreate, async (interaction) => {
         if (!interaction.isChatInputCommand()){return};
-        if (interaction.member.roles.cache.has('1253312057848365086')){
-            console.log("Certified pookie!")
-            let query = read(client.mongodb, { guildid: interaction.guildid })
-            console.log(query)
+        let query = await read(client.mongodb, { guildid: interaction.guild.id })
+        console.log(query) 
             /*
             Plan:
             read command name and query to mongodb
             check if user has any of the allowed roles
             */
-        }
+        
         let command = interaction.client.commands.get(interaction.commandName);
         if (!command){
             console.log(`Did not find: ${interaction.commandName} !`)
