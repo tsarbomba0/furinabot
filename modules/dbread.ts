d563b35fb9b80f51bcd4917d0b91c8053faee49b
tsarbomba0 <creepermaster000@gmail.com>
2024-10-15 22:15:15 +0200

    dbread command

---
+++ modules/dbread.ts
@@ -0,0 +1,59 @@
+import { SlashCommandBuilder, EmbedBuilder, Embed, ColorResolvable } from "discord.js";
+import { dbfind } from '../util/mongodb'
+const config = require('../config.json')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("dbread")
+    .setDescription("Reads roles allowed for a command")
+    .addStringOption(option =>
+        option.setName("command")
+        .setRequired(true)
+        .setDescription("Reads roles that are allowed to execute a command")
+    ),
+    async execute(interaction){
+        let commandName = interaction.options.getString('command')
+
+        // Projection for MongoDB, show only the queried command as response
+        let projection = {
+            _id: 0
+        }
+        projection[commandName] = 1
+
+        // Query
+        let query_options = {
+            projection
+        }
+        let query = await dbfind(interaction.client.mongodb, { guildid: interaction.guild.id }, query_options)
+        
+        // Check if any roles were set
+        if(Object.keys(query).length === 0){
+            await interaction.reply({ content: "No allowed roles set for this command!", ephemeral: true})
+        } else {
+            // Evaluate roles from ids id -> role name from cache
+            let roleIDArray: Array<string> = Object.values(query)[0].split(',');
+            let roleNameString: string;
+
+            roleIDArray.forEach(roleid => {
+                let roleName = interaction.guild.roles.cache.get(roleid).name
+                if (!roleNameString){
+                    roleNameString = roleName
+                } else {
+                    roleNameString = roleNameString.concat(', ', roleName)
+                }
+                
+            })
+            
+            // Embed
+            const Embed = new EmbedBuilder()
+            .setTitle(`Roles set for: /${commandName}`)
+            .setDescription(roleNameString)
+            .setColor(config.embed_color as ColorResolvable)
+            .setTimestamp()
+            .setFooter({ text: "Hell yeah!"})
+
+            // Reply
+            await interaction.reply({ embeds: [Embed], ephemeral: true})
+        }
+    }
+}
