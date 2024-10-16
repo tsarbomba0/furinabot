681012d92ca86cb053c4107da341a9955480f6f5
tsarbomba0 <creepermaster000@gmail.com>
2024-10-16 22:49:35 +0200

    renovated 'homework' command c:

--- modules/hw.ts
+++ modules/hw.ts
@@ -2,12 +2,12 @@ const { SlashCommandBuilder } = require("discord.js");
 const { EmbedBuilder } = require("discord.js");
 const config = require('../config.json')
 const axios = require('axios');
-const parseString = require('xml2js').parseString;
+const cheerio = require('cheerio')
 
 module.exports = {
     data: new SlashCommandBuilder()
-    .setName('yuri')
-    .setDescription('Sends yuri content')
+    .setName('hw')
+    .setDescription('Homework.')
     .addStringOption((option: { setName: (arg0: string) => { (): any; new(): any; setDescription: { (arg0: string): any; new(): any; }; }; }) => 
         option.setName('victim')
             .setDescription('Character name.')
@@ -17,11 +17,8 @@ module.exports = {
         option.setName('tags')
             .setDescription('Tags separated by spaces (example: yuri video futa -ai_generated)')
     ),
-
-
     async execute(interaction){
         // variables
-        let author = interaction.user.globalName
         let victim = interaction.options.getString('victim').toLowerCase()
         let tags = interaction.options.getString('tags')
         let taglist = "";
@@ -30,7 +27,8 @@ module.exports = {
         try {
             await interaction.deferReply()
         } catch (err) {
-            await interaction.reply({ content: "Something really went wrong!", ephemeral: true})
+            //await interaction.reply({ content: "Something really went wrong!", ephemeral: true})
+            console.log("uhh!")
             return;
         }
 
@@ -46,44 +44,48 @@ module.exports = {
         let data;
         try {
             response = await axios.get(`https://api.rule34.xxx/index.php?page=dapi&s=tag&q=index&limit=25&json=1&name_pattern=${victim.replace(" ", "_")}%&order=count`)
-            console.log(victim.replace(" ", "_"))
             data = await response.data
         } catch (err){
-            interaction.editReply({ content: "Couldn't connect with the Rule34 API!", ephemeral: true});
+            interaction.editReply({ content: "Couldn't connect with the Rule34 API!"});
             return;
         }
 
-        console.log(data)
-        // parse XML
-        parseString(data, async (err: any, result)=>{
-            // on error
-            if(err){
-                interaction.editReply({ content: "Something went wrong during parsing the output from Rule34!", ephemeral: true})
-            }
-
-            // character name from highest count tag
-            let charname;
-            try {
-                charname = result.tags.tag[result.tags.tag.length - 1].$.name
-            } catch (err){
-                console.log(err)
-                await interaction.editReply({ content: "Couldn't find that!", ephemeral: true})
-                return;
-            }
-            // response from API regarding a query for charname and the taglist
-            response = await axios.get(`https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&limit=100&json=1&tags=${charname}${taglist}+-ai_generated `) // maybe cache using hash?
+        // Parse XML using Cheerio 
+        const parsed_xml = cheerio.load(data, {
+            xml: true
+        })
+        parsed_xml('tags').get().map(async(arg) =>{
+            let highestCount = 0;
+            let highestCountTag: string;
+            arg.children.forEach(element => {
+                if(element.attribs){
+                    console.log(`${element.attribs.name}, ${element.attribs.count}`)
+                    if(element.attribs.count > highestCount){
+                        highestCount = element.attribs.count
+                        highestCountTag = element.attribs.name
+                    } 
+                    
+                }
+            })
+            response = await axios.get(`https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&limit=100&json=1&tags=${highestCountTag}${taglist}+-ai_generated+-video `) // maybe cache using hash?
             // random post from array
             let random_post = response.data[Math.floor(response.data.length * Math.random())]
+
+            // debug for later
+            console.log(random_post)
             console.log(random_post.file_url)
+            
             // embed
             const Embed = new EmbedBuilder()
                 .setColor(config.embed_color)
                 .setFooter({ text: "lovely....!"})
                 .setTimestamp()
-                .setTitle(`${author}! You asked for this.`)
+                .setTitle(`${interaction.user.globalName}! You asked for this.`)
                 .setImage(random_post.file_url)
             if(tags !== null)Embed.setDescription(`Tags: ${tags}`) // if tags are null (no tags typed in), doesn't add a description
             await interaction.editReply({ embeds: [Embed]}) // edits the deferred reply
-        })   
+            return;
+        })
+        await interaction.editReply({ content: "Couldn't find that!" }) 
     } 
 }
